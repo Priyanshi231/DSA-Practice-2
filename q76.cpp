e65e707027623064b52b095f692f6716de51929d
Priyanshi Jain <163301165+Priyanshi231@users.noreply.github.com>
2026-07-22 11:01:27 +0530

    Update q76.cpp
    Implemented Boyer-Moore algorithm to find majority elements in the array.

--- q76.cpp
+++ q76.cpp
@@ -1,3 +1,5 @@
+//more time complexity
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
@@ -22,3 +24,58 @@ public:
 
 
 
+//Boyer-Moore algorithm
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int cand1 = 0, cand2 = 1;
+        int c1 = 0, c2=0;;
+
+        for(int num : nums){
+            if(num == cand1){
+                c1++;
+            }
+            else if(num == cand2){
+                c2++;
+            }
+            else if(c1 == 0){
+                cand1 = num;
+                c1 = 1;
+            }
+            else if(c2 == 0){
+                cand2 = num;
+                c2 = 1;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+
+        c1 = 0;
+        c2 = 0;
+
+        for(int num : nums){
+            if(num == cand1){
+                c1++;
+            }
+            else if(num == cand2){
+                c2++;
+            }           
+        }
+
+        vector<int> ans;
+
+        if(c1 > n/3){
+            ans.push_back(cand1);
+        }
+        if(c2 > n/3){
+            ans.push_back(cand2);
+        }
+
+        return ans;
+    }
+};
